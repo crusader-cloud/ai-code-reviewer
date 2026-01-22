dd66cca93bd37150c581c80880fa8d4e1ab7aeed
crusader-cloud <btech10300.25@bitmesra.ac.in>
2026-01-22 17:09:49 +0530

    Update test.py for PR review

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-def add(a,b):
-    return a+b
 
-for i in range(10):
-    print(add(i,i))
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+for i in range(5):
+    print(add(i, i))
+    print(multiply(i, i))
